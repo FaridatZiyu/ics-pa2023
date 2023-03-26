6bfd748ee85b9dd8340ea6f2a51182ed52cca377
tracer-ics2017 <tracer@njuics.org>
2023-03-26 16:45:14 +0800

    >  compile 1911494 faridat Linux faridat-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:45:14 up 56 min,  1 user,  load average: 1.42, 1.10, 1.00 ae0420c0a6360d4fcba18fb46d6863bdbce82352

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -6,3 +6,16 @@ make_EHelper(operand_size);
 
 make_EHelper(inv);
 make_EHelper(nemu_trap);
+
+// data-mov.c
+make_EHelper(mov);
+make_EHelper(push);
+make_EHelper(pop);
+make_EHelper(pusha);
+make_EHelper(popa);
+make_EHelper(leave);
+make_EHelper(cltd);
+make_EHelper(cwtl);
+make_EHelper(movsx);
+make_EHelper(movzx);
+make_EHelper(lea);
