cd993a8e4464f5fcdea9600e8e8be49122f934be
tracer-ics2017 <tracer@njuics.org>
2023-05-11 19:02:30 +0800

    >  compile 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:02:30 up  1:39,  1 user,  load average: 0.10, 0.17, 0.23 fce1e6612228e6d773b99b11941a21d1c66cd9f4

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -52,6 +52,7 @@ typedef struct {
     uint16_t limit;
   } idtr;
   
+  rtlreg_t cs;
 } CPU_state;
 
 extern CPU_state cpu;
--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -82,6 +82,7 @@ static inline void load_img() {
 static inline void restart() {
   /* Set the initial instruction pointer. */
   cpu.eip = ENTRY_START;
+  cpu.cs = 8;
 
   unsigned int origin=2;
   memcpy(&cpu.eflags, &origin, sizeof(cpu.eflags));
