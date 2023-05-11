fab00e9511271b816db4285d765d71953f1dbc7f
tracer-ics2017 <tracer@njuics.org>
2023-05-11 21:43:50 +0800

    >  compile 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:43:50 up  2:09,  1 user,  load average: 0.56, 0.72, 0.56 22c2eaff43a961a9e0158036aadaf2a66099185f

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -190,7 +190,7 @@ opcode_entry opcode_table [512] = {
   /* 0xc0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc4 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc8 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xcc */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0xcc */	EMPTY, IDEXW(I,int,1), EMPTY, EMPTY,
   /* 0xd0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xd4 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xd8 */	EMPTY, EMPTY, EMPTY, EMPTY,
