ce031793a7dc6577313b3f5d66a7bc9f49855a60
tracer-ics2017 <tracer@njuics.org>
2023-05-11 14:19:17 +0800

    >  run 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:19:17 up  2:04,  1 user,  load average: 0.54, 0.48, 0.27 9959694a1b8afb6514b3b38268d01166fadb296

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,12 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+#define RAMDISK_SIZE ((&ramdisk_start) - (&ramdisk_end))
+extern void ramdisk_read(void *buf, off_t offser, size_t len);
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  ramdisk_read(DEFAULT_ENTRY, 0, RAMDISK_SIZE);
   return (uintptr_t)DEFAULT_ENTRY;
 }
