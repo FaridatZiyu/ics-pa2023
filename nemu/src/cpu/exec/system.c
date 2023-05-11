3694c03fe9f9fdf2b364f8c8eef0a3e862b6b42f
tracer-ics2017 <tracer@njuics.org>
2023-05-11 22:09:46 +0800

    >  compile 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:09:46 up  2:35,  1 user,  load average: 0.53, 0.38, 0.45 1b6888492325bf2db1cef27ee9dabc8b0bce5be

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -38,8 +38,8 @@ make_EHelper(mov_cr2r) {
 
 extern void raise_intr(uint8_t NO, vaddr_t ret_addr);
 make_EHelper(int) {
-  TODO();
   uint8_t NO = id_dest->val & 0xff;
+  printf("%d", NO);
   raise_intr(NO, decoding.seq_eip);
 
   print_asm("int %s", id_dest->str);
