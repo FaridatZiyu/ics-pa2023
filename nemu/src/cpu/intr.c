16299ad743b58dc2cd850ed4233147cdf7706653
tracer-ics2017 <tracer@njuics.org>
2023-05-11 22:32:05 +0800

    >  compile 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:32:05 up  2:57,  1 user,  load average: 0.68, 0.34, 0.28 be31d68eaab03993b0c4e60ad1fafb45fc7b6cd

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,22 +6,35 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
+  // memcpy(&t1, &cpu.eflags, sizeof(cpu.eflags));
+  // rtl_li(&t0, t1);
+  // rtl_push(&t0);
+  // rtl_push(&cpu.cs);
+  // rtl_li(&t0, ret_addr);
+  // rtl_push(&t0);
+  
+  // vaddr_t gate_addr = cpu.idtr.base+NO*sizeof(GateDesc);
+  // // assert(gate_addr<=cpu.idtr.base+cpu.idtr.limit);
+
+  // uint32_t off_15_0 = vaddr_read(gate_addr, 2);
+  // uint32_t off_32_16 = vaddr_read(gate_addr+sizeof(GateDesc)-2, 2);
+  // uint32_t target_addr = (off_32_16<<16) + off_15_0;
+
   memcpy(&t1, &cpu.eflags, sizeof(cpu.eflags));
   rtl_li(&t0, t1);
   rtl_push(&t0);
+  cpu.eflags.IF = 0;
   rtl_push(&cpu.cs);
   rtl_li(&t0, ret_addr);
   rtl_push(&t0);
-  
-  vaddr_t gate_addr = cpu.idtr.base+NO*sizeof(GateDesc);
-  // assert(gate_addr<=cpu.idtr.base+cpu.idtr.limit);
+  rtl_li(&t0, vaddr_read(cpu.idtr.base + NO * 8, 4));
+  rtl_li(&t1, vaddr_read(cpu.idtr.base + NO * 8 + 4, 4));
+  assert(t1 & 0x00008000);
+  uint32_t target_addr = (t0 & 0xffff) | (t1 & 0xffff0000);
 
-  uint32_t off_15_0 = vaddr_read(gate_addr, 2);
-  uint32_t off_32_16 = vaddr_read(gate_addr+sizeof(GateDesc)-2, 2);
-  uint32_t target_addr = (off_32_16<<16) + off_15_0;
-#ifdef DEBUG
+// #ifdef DEBUG
   Log("target_addr=0x%x", target_addr);
-#endif
+// #endif
 
   decoding.is_jmp = 1;
   decoding.jmp_eip = target_addr;
