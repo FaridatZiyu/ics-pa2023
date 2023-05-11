e128c334f9dc0e07751b52e707586969552ca168
tracer-ics2017 <tracer@njuics.org>
2023-05-11 21:45:31 +0800

    >  compile 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:31 up  2:10,  1 user,  load average: 0.46, 0.64, 0.54 23b5e08553549b429f100cc5f4e5f1ad346c54c2

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -14,6 +14,8 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&t0);
   
   vaddr_t gate_addr = cpu.idtr.base+NO*sizeof(GateDesc);
+  Log("NO:%d", NO);
+  Log("sizeof(GateDesc):%d", sizeof(GateDesc));
   assert(gate_addr<=cpu.idtr.base+cpu.idtr.limit);
 
   uint32_t off_15_0 = vaddr_read(gate_addr, 2);
