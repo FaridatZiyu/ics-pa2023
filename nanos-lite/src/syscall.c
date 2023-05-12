ed4a1da8cd19b130a149774164a04e037b46d5c8
tracer-ics2017 <tracer@njuics.org>
2023-05-12 23:01:04 +0800

    >  run 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:01:04 up  1:30,  1 user,  load average: 0.29, 0.49, 0.49 f0f0bddea660846d92856784e7aba50ec262e30f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,26 @@
 #include "common.h"
 #include "syscall.h"
 
+static inline _RegSet* sys_none(_RegSet *r) {
+  SYSCALL_ARG1(r) = 1;
+  return NULL;
+}
+
+static inline _RegSet* sys_exit(_RegSet *r) {
+  _halt(SYSCALL_ARG2(r));
+  return NULL;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
+    case SYS_none: return sys_none(r);
+    case SYS_exit: return sys_exit(r);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
