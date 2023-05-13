73eddc429033509abad88e616fe3a26b9f052ea4
tracer-ics2017 <tracer@njuics.org>
2023-05-13 21:35:44 +0800

    >  run 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:35:44 up 42 min,  1 user,  load average: 0.38, 0.53, 0.54 c470ea9af84497c0a0b1645a51fe019592794d50

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,17 +11,29 @@ static inline _RegSet* sys_exit(_RegSet *r) {
   return NULL;
 }
 
-_RegSet* do_syscall(_RegSet *r) {
-  uintptr_t a[4];
-  a[0] = SYSCALL_ARG1(r);
-  a[1] = SYSCALL_ARG2(r);
-  a[2] = SYSCALL_ARG3(r);
-  a[3] = SYSCALL_ARG4(r);
+static inline _RegSet* sys_write(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  char* buf = (char*)SYSCALL_ARG3(r);
+  size_t count = (size_t)SYSCALL_ARG4(r);
+  if (fd == 1 || fd == 2) {
+    for(int i = 0; i < count; i++){
+      _putc(buf[i]);
+    }
+    SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
+  } else {
+    panic("Unhandled fd = %d in sys_write.\n", fd);
+    SYSCALL_ARG1(r) = -1;
+  }
+  return NULL; 
+}
 
-  switch (a[0]) {
-    case SYS_none: return sys_none(r);
-    case SYS_exit: return sys_exit(r);
-    default: panic("Unhandled syscall ID = %d", a[0]);
+_RegSet* do_syscall(_RegSet *r) {
+  switch (SYSCALL_ARG1(r)) {
+  case SYS_none:  return sys_none(r);
+  case SYS_exit:  return sys_exit(r);
+  case SYS_write: return sys_write(r);
+  default:
+    panic("Unhandled syscall ID = %d", SYSCALL_ARG1(r));
   }
 
   return NULL;
