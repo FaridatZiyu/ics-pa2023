900f2d121c1d79b0eebcbecef5e68b4d47f9c4c2
tracer-ics2017 <tracer@njuics.org>
2023-05-14 21:53:44 +0800

    >  run 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:53:44 up  2:58,  1 user,  load average: 0.65, 0.37, 0.30 e7ea771d5e30b7e56bd12152d23a113a0f0f26

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -4,6 +4,7 @@ typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -21,6 +22,60 @@ static Finfo file_table[] __attribute__((used)) = {
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern void ramdisk_write(void *buf, off_t offset, size_t len);
+
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+size_t fs_filesz(int fd) {
+  assert(fd>=0 && fd<NR_FILES);
+  return file_table[fd].size;
+}
+
+off_t disk_offset(int fd) {
+  assert(fd>=0 && fd<NR_FILES);
+  return file_table[fd].disk_offset;
+}
+
+off_t get_open_offset(int fd) {
+  assert(fd>=0 && fd<NR_FILES);
+  return file_table[fd].open_offset;
+}
+
+void set_open_offset(int fd, off_t n) {
+  assert(fd>=0 && fd<NR_FILES);
+  assert(n>=0);
+  if(n > file_table[fd].size)
+    n = file_table[fd].size;
+  file_table[fd].open_offset = n;
+}
+
+int fs_open(const char* filenane, int flags, int mode) {
+  for(int i=0; i<NR_FILES; i++) {
+    if(strcmp(filenane, file_table[i].name)==0)
+      return i;
+  }
+  panic("This filename not exist in file_table");
+  return -1;
+}
+
+ssize_t fs_read(int fd, void* buf, size_t len) {
+  assert(fd>=0 && fd<NR_FILES);
+  if(fd < 3) {
+    Log("arg invaid:fd<3");
+    return 0;
+  }
+  int n = fs_filesz(fd) - get_open_offset(fd);
+  if(n > len)
+    n = len;
+  ramdisk_read(buf, disk_offset(fd)+get_open_offset(fd), n);
+  set_open_offset(fd, get_open_offset(fd)+n);
+  return n;
+}
+
+int fs_close(int fd) {
+  assert(fd>=0 && fd<NR_FILES);
+  return 0;
 }
