d7d18ab5bd090fd44d594c02c3bce2b08a39b6c5
tracer-ics2017 <tracer@njuics.org>
2023-05-14 22:26:06 +0800

    >  run 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:26:06 up  3:31,  1 user,  load average: 0.91, 0.65, 0.46 cc7513463e98bde93d413b9f4b83de7e93ae2b8

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -4,7 +4,7 @@ typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
-  off_t open_offset;
+  // off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -22,63 +22,63 @@ static Finfo file_table[] __attribute__((used)) = {
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
-extern void ramdisk_read(void *buf, off_t offset, size_t len);
-extern void ramdisk_write(void *buf, off_t offset, size_t len);
+// extern void ramdisk_read(void *buf, off_t offset, size_t len);
+// extern void ramdisk_write(void *buf, off_t offset, size_t len);
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
-  file_table[FD_FB].size = _screen.height * _screen.width * 4;
+  // file_table[FD_FB].size = _screen.height * _screen.width * 4;
 }
 
-size_t fs_filesz(int fd) {
-  assert(fd>=0 && fd<NR_FILES);
-  return file_table[fd].size;
-}
+// size_t fs_filesz(int fd) {
+//   assert(fd>=0 && fd<NR_FILES);
+//   return file_table[fd].size;
+// }
 
-off_t disk_offset(int fd) {
-  assert(fd>=0 && fd<NR_FILES);
-  return file_table[fd].disk_offset;
-}
+// off_t disk_offset(int fd) {
+//   assert(fd>=0 && fd<NR_FILES);
+//   return file_table[fd].disk_offset;
+// }
 
-off_t get_open_offset(int fd) {
-  assert(fd>=0 && fd<NR_FILES);
-  return file_table[fd].open_offset;
-}
+// off_t get_open_offset(int fd) {
+//   assert(fd>=0 && fd<NR_FILES);
+//   return file_table[fd].open_offset;
+// }
 
-void set_open_offset(int fd, off_t n) {
-  assert(fd>=0 && fd<NR_FILES);
-  assert(n>=0);
-  if(n > file_table[fd].size)
-    n = file_table[fd].size;
-  file_table[fd].open_offset = n;
-}
+// void set_open_offset(int fd, off_t n) {
+//   assert(fd>=0 && fd<NR_FILES);
+//   assert(n>=0);
+//   if(n > file_table[fd].size)
+//     n = file_table[fd].size;
+//   file_table[fd].open_offset = n;
+// }
 
-int fs_open(const char* filename, int flags, int mode) {
-  for(int i=0; i<NR_FILES; i++) {
-    if(strcmp(filename, file_table[i].name)==0) {
-      file_table[i].open_offset = 0;
-      return i;
-    }
-  }
-  panic("This filename not exist in file_table");
-  return -1;
-}
+// int fs_open(const char* filename, int flags, int mode) {
+//   for(int i=0; i<NR_FILES; i++) {
+//     if(strcmp(filename, file_table[i].name)==0) {
+//       file_table[i].open_offset = 0;
+//       return i;
+//     }
+//   }
+//   panic("This filename not exist in file_table");
+//   return -1;
+// }
 
-ssize_t fs_read(int fd, void* buf, size_t len) {
-  assert(fd>=0 && fd<NR_FILES);
-  if(fd < 3) {
-    Log("arg invaid:fd<3");
-    return 0;
-  }
-  int n = fs_filesz(fd) - get_open_offset(fd);
-  if(n > len)
-    n = len;
-  ramdisk_read(buf, disk_offset(fd)+get_open_offset(fd), n);
-  set_open_offset(fd, get_open_offset(fd)+n);
-  return n;
-}
+// ssize_t fs_read(int fd, void* buf, size_t len) {
+//   assert(fd>=0 && fd<NR_FILES);
+//   if(fd < 3) {
+//     Log("arg invaid:fd<3");
+//     return 0;
+//   }
+//   int n = fs_filesz(fd) - get_open_offset(fd);
+//   if(n > len)
+//     n = len;
+//   ramdisk_read(buf, disk_offset(fd)+get_open_offset(fd), n);
+//   set_open_offset(fd, get_open_offset(fd)+n);
+//   return n;
+// }
 
-int fs_close(int fd) {
-  assert(fd>=0 && fd<NR_FILES);
-  return 0;
-}
+// int fs_close(int fd) {
+//   assert(fd>=0 && fd<NR_FILES);
+//   return 0;
+// }
