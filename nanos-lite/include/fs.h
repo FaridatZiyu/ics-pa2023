89040875f50ef321c215aa80e34fde29340d7abf
tracer-ics2017 <tracer@njuics.org>
2023-05-14 22:28:44 +0800

    >  run 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:28:44 up  3:33,  1 user,  load average: 1.79, 0.91, 0.57 2cdd43162cce49a6bd226464c1415b0fc361476

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -5,11 +5,11 @@
 
 enum {SEEK_SET, SEEK_CUR, SEEK_END};
 
-// size_t fs_filesz(int fd);
-// int fs_open(const char* filenane, int flags, int mode);
-// ssize_t fs_read(int fd, void* buf, size_t len);
-// ssize_t fs_write(int fd, void* buf, size_t len);
-// int fs_close(int fd);
-// off_t fs_lseek(int fd, off_t offset, int whence);
+size_t fs_filesz(int fd);
+int fs_open(const char* filenane, int flags, int mode);
+ssize_t fs_read(int fd, void* buf, size_t len);
+ssize_t fs_write(int fd, void* buf, size_t len);
+int fs_close(int fd);
+off_t fs_lseek(int fd, off_t offset, int whence);
 
 #endif
