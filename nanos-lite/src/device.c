25200e269a3a7cf2a2ec6b46af7c33ec599e084d
tracer-ics2017 <tracer@njuics.org>
2023-05-15 22:32:49 +0800

    >  run 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:32:49 up  5:53,  1 user,  load average: 0.27, 0.27, 0.24 dd0b3a44c26674f466a7c8425ea64f6d28edc3b1

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -19,6 +19,7 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+  Log("width:%d",_screen.width);
   int screen_y1 = offset / 4 / _screen.width;
   int screen_x1 = offset / 4 % _screen.width;
   int screen_y2 = (offset+len) / 4 / _screen.width;
