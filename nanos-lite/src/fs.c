a8a070c7c8f9286742ef177a82fe3c6403f1affe
tracer-ics2017 <tracer@njuics.org>
2023-05-16 00:09:16 +0800

    >  run 1911494 faridat Linux faridat-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:09:16 up  7:30,  1 user,  load average: 0.30, 0.29, 0.25 8b14a8351c4907c1b9c0971b7641fa7e401cb53b

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -57,7 +57,7 @@ void set_open_offset(int fd, off_t n) {
 int fs_open(const char* filename, int flags, int mode) {
   for(int i=0; i<NR_FILES; i++) {
     if(strcmp(filename, file_table[i].name)==0) {
-      Log("Floading %s", filename);
+      Log("loading %s", filename);
       file_table[i].open_offset = 0;
       return i;
     }
