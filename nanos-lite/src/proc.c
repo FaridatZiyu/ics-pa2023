017db396b99b5f624dd0764586c984fb09acdb6f
tracer-ics2017 <tracer@njuics.org>
2023-05-28 19:31:50 +0800

    >  run 1911494 faridat Linux faridat-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:31:50 up 15 min,  1 user,  load average: 1.39, 1.06, 0.84 4f0eb69e9325038ee255d66257d9ee88b724532

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,7 @@ void load_prog(const char *filename) {
 
 _RegSet* schedule(_RegSet *prev) {
   current->tf = prev;
-  current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   _switch(&current->as);
   return current->tf;
 }
